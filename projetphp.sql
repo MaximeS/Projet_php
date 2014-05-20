4aaca341c5016c7a7ad1f5149de7852b010eae5b
valgwen <valgwen@laposte.net>
2014-05-20 11:24:23 +0200

    base données finie... ou presque

--- projetphp.sql
+++ projetphp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le: Mar 06 Mai 2014 à 09:34
--- Version du serveur: 5.6.12-log
--- Version de PHP: 5.4.16
+-- Client :  127.0.0.1
+-- Généré le :  Mar 20 Mai 2014 à 11:23
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `projetphp`
+-- Base de données :  `projetphp`
 --
-CREATE DATABASE IF NOT EXISTS `projetphp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `projetphp`;
 
 -- --------------------------------------------------------
 
@@ -30,8 +28,7 @@ USE `projetphp`;
 
 CREATE TABLE IF NOT EXISTS `chat` (
   `chat_id` int(11) NOT NULL AUTO_INCREMENT,
-  `dqs` int(11) NOT NULL,
-  `sdq` int(11) NOT NULL,
+  `chat_text` varchar(50) NOT NULL,
   PRIMARY KEY (`chat_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -42,11 +39,43 @@ CREATE TABLE IF NOT EXISTS `chat` (
 --
 
 CREATE TABLE IF NOT EXISTS `idee` (
-  `idee_id` int(11) NOT NULL AUTO_INCREMENT,
-  `text` text NOT NULL,
-  `pseudo` varchar(20) NOT NULL,
-  PRIMARY KEY (`idee_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `idee_id` int(11) NOT NULL,
+  `idee_text` tinytext,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idee_id`),
+  KEY `idee` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `news_id` int(11) NOT NULL,
+  `news_title` varchar(100) DEFAULT NULL,
+  `News_text` varchar(3000) DEFAULT NULL,
+  `News_type` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `News_date` date NOT NULL,
+  PRIMARY KEY (`news_id`),
+  KEY `news` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reponse`
+--
+
+CREATE TABLE IF NOT EXISTS `reponse` (
+  `sujet_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `reponse_text` text,
+  KEY `sujet_id` (`sujet_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,12 +84,13 @@ CREATE TABLE IF NOT EXISTS `idee` (
 --
 
 CREATE TABLE IF NOT EXISTS `sujets` (
-  `sujets_id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(100) NOT NULL,
-  `resolu` tinyint(1) NOT NULL,
-  `pseudo` varchar(20) NOT NULL,
-  PRIMARY KEY (`sujets_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `sujet_id` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `resolu` tinyint(4) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`sujet_id`),
+  KEY `sujets` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -72,10 +102,61 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pseudo` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
   `mail` varchar(100) NOT NULL,
-  `Avatar` varchar(100) NOT NULL,
+  `niveau` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`user_id`),
   UNIQUE KEY `pseudo` (`pseudo`),
   UNIQUE KEY `mail` (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_chat`
+--
+
+CREATE TABLE IF NOT EXISTS `user_chat` (
+  `user_id` int(11) DEFAULT NULL,
+  `chat_id` int(11) DEFAULT NULL,
+  KEY `user_chat` (`user_id`),
+  KEY `idee` (`chat_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `idee`
+--
+ALTER TABLE `idee`
+  ADD CONSTRAINT `idee_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Contraintes pour la table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `news_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Contraintes pour la table `reponse`
+--
+ALTER TABLE `reponse`
+  ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`sujet_id`) REFERENCES `sujets` (`sujet_id`),
+  ADD CONSTRAINT `reponse_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Contraintes pour la table `sujets`
+--
+ALTER TABLE `sujets`
+  ADD CONSTRAINT `sujets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Contraintes pour la table `user_chat`
+--
+ALTER TABLE `user_chat`
+  ADD CONSTRAINT `user_chat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `user_chat_ibfk_2` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`chat_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
